d2346820663c70f806ed6e38f5355b2d8d3364ae
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-01-17 16:32:56 +0000

    Collect sales data for last 5 markets in list of columns

--- run.py
+++ run.py
@@ -38,6 +38,23 @@ def get_sales_data():
     return sales_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
+
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -120,4 +137,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+#main()
+
+sales_columns = get_last_5_entries_sales()
